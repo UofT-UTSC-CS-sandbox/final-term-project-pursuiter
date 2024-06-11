ac6351b14f632f0a104a0b65cc222f0deee25e19
DavidIskandaryan <144949152+DavidIskandaryan@users.noreply.github.com>
2024-06-11 19:33:37 -0400

    David pur 22 (#25)
    * changed file name to work with windows
    
    * frontend for personal information
    
    * backend included finalized version
    
    * final version including tests
    
    * Prettier
    
    * fully completed landing page, login add link
    
    * edited login page and landing page
    
    * backend included finalized version
    
    * Prettier
    
    * fully completed landing page, login add link
    
    * edited login page and landing page

--- frontend/src/components/landing-page/LandingPage.js
+++ frontend/src/components/landing-page/LandingPage.js
@@ -2,19 +2,54 @@ import "./LandingPage.css";
 
 import React from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
+  const handleJobSeeker = () => {
+    navigate("/applicant-signup");
+  };
+
+  const handleRecruiter = () => {
+    navigate("/recruiter-signup");
+  };
+
   return (
-    <div className="landing-container">
-      <h1>Landing Page</h1>
-      <div>
-        <Link to="/login">Login</Link>
-      </div>
-      <div>
-        <Link to="/applicant-signup">Job Seeker Signup</Link>
-      </div>
-      <div>
-        <Link to="/recruiter-signup">Recruiter Signup</Link>
+    <div className="landing-page-container">
+      <header className="landing-page-header">
+        <div className="logo">PERSUITER</div>
+        <div className="header-links">
+          <div>
+            <Link className="login-link" to="/login">
+              LOGIN
+            </Link>
+          </div>
+        </div>
+      </header>
+      <div className="content-container">
+        <div className="column-left">
+          <h1>Find a job you love.</h1>
+          <p className="left-paragraph">
+            Take full advantage of our advanced job matching system and AI
+            powered application recommendations. Your dream job is just a few
+            clicks away.
+          </p>
+          <button onClick={handleJobSeeker} className="landing-page-button">
+            Join as Job-Seeker
+          </button>
+        </div>
+        <div className="column-right">
+          <h1>Attract. Select. Grow.</h1>
+          <p className="right-paragraph">
+            Tired of applicants that dont match your postings? Not with us.
+            Pursuiter makes sure your postings get only the best candidates and
+            helps filter them using AI.
+          </p>
+          <button onClick={handleRecruiter} className="landing-page-button">
+            Join as Recruiter
+          </button>
+        </div>
       </div>
     </div>
   );
