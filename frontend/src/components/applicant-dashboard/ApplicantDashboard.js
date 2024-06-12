cdb83c22d5420a213836a8ddab748fff86d74c07
Katherine Pravdin <katherinepravdin@gmail.com>
2024-06-11 20:20:52 -0400

    rebased with dev

--- frontend/src/components/applicant-dashboard/ApplicantDashboard.js
+++ frontend/src/components/applicant-dashboard/ApplicantDashboard.js
@@ -80,7 +80,8 @@ function ApplicantDashboard() {
               <div className="header-links">
 <<<<<<< HEAD
                   <div className="header-link">Jobs</div>
-                  <div className="header-link">ACCOUNT</div>
+                  {/* <div className="header-link" navigateTo="/applicant-information" >ACCOUNT</div> */}
+                  <div className="header-link" onClick={() => navigate("/applicant-information")}>ACCOUNT</div>
                   <div className="header-link" onClick={handleLogout}>LOGOUT</div>
 =======
                   <div className="header-link">Postings</div>
