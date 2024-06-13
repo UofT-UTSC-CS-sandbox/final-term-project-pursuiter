56de195c093f9211024ee12dbe0293b26c9cee05
Katherine Pravdin <katherinepravdin@gmail.com>
2024-06-13 12:38:22 -0400

    deleted unneeded files

--- backend/model/imagesSchema.js
+++
@@ -1,9 +0,0 @@
-// import mongoose from 'mongoose';
-
-// const PostSchemaOne = new mongoose.Schema({
-
-// }, { strict: false });
-
-// let imagesSchema = mongoose.model('binaryimages', PostSchemaOne);
-// // module.exports = imagesSchema;
-// export default imagesSchema;
